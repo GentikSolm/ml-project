7133ea8b1c7d72f6b5c825837b6684f600130490
Josh Brown <josh@mjbrowns.com>
2022-11-17 23:07:38 -0500

    feat: add etl script

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# ml-project
+# ML-Project
+
+Total records
+- 12781
+
+Response Variable
+- Return on Investment (roi)
+
+Dependant Variables
+
+|-------|------|
+| fbafees | Fees associated with reselling on amazon |
+| newprice | Current new price of product |
+| oosamazon | Amazon out of stock percentage |
+| ooslistprice | List price out of stock percentage |
+| avgamazon | 180 day average amazon price |
+| avglistprice | 180 day average list price |
+| newPriceIsMap | If the current price is MAP price |
+| isAdultProduct | If the product is an adult product |
+| productType | The type of the product |
+
+
+# Product Types
+
+Values for productType
+
+|---|-------------|
+| 0 | Standard |
+| 1 | Downloadable |
+| 2 | EBook |
+| 3 | Inaccessable |
+| 4 | Invalid |
+| 5 | Variation Parent |
---
+++ etl.py
@@ -0,0 +1,57 @@
+from dotenv import dotenv_values
+from pymongo import MongoClient
+
+SECRETS = dotenv_values('.env')
+
+client = MongoClient(SECRETS["MONGO_URL"])
+db = client.abot
+
+def prune(var):
+    return var if var != -1 else ""
+
+asins = db.asin.find()
+with open("asins.csv", "w") as data:
+    for asin_doc in asins:
+        print(asin_doc['asin'])
+        asin = asin_doc.get('keepa_data')
+        if(asin == None):
+            continue
+        stats = asin.get('stats')
+        if(stats == None):
+            continue
+
+        roi = round(asin_doc['roi'], 2)
+
+        fbaFees = asin.get('fbaFees')
+        if(fbaFees != None):
+            fbaFees = fbaFees['pickAndPackFee']
+        if(fbaFees == -1 or fbaFees == None):
+            fbaFees = 0
+        variations = asin.get('variations')
+        if(variations != None):
+            variations = len(variations)
+        if(variations == None):
+            variations = 0
+
+        current = stats['current']
+        amazon = prune(current[0])
+        new = prune(current[1])
+        used = prune(current[2])
+        listprice = prune(current[4])
+        oos = stats['outOfStockPercentage90']
+        oosamazon = prune(oos[0])
+        oosused = prune(oos[2])
+        ooslistprice = prune(oos[4])
+        avg180 = stats['avg180']
+        avgamazon = prune(avg180[0])
+        avgused = prune(avg180[2])
+        avglistprice = prune(avg180[4])
+
+        newPriceIsMap = int(asin['newPriceIsMAP'])
+        isAdultProduct = int(asin['isAdultProduct'])
+        productType = asin['productType']
+
+        line = f"{roi},{fbaFees},{new},{oosamazon},{ooslistprice},{avgamazon},{avglistprice},{newPriceIsMap},{isAdultProduct},{productType}"
+        data.write(line)
+        data.write("\n")
+        print(line)
