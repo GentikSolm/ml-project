6d4feb545aed1ae3ef8e0707f27026da59dd5953
jake052799 <101911681+jake052799@users.noreply.github.com>
2022-11-26 23:17:13 -0500

    Add files via upload

--- Final Project R Code 2.R
+++ Final Project R Code 2.R
@@ -250,7 +250,7 @@ plot(Train.fitted, as.matrix(Train.resp), xlab="Fitted", ylab="Actual")
 lines(Test.pred, as.matrix(Test.resp), type="p", col="red", pch=19)
 abline(0,1, col="red")
 
-library(caret)          # install.packages("caret")
+library(caret)        
 RandomForest <- data.frame(
   tr.RMSE      = caret::RMSE(Train.fitted, as.matrix(Train.resp)),
   tr.Rsquare   = caret::R2(Train.fitted, Train.resp),
@@ -267,8 +267,6 @@ treeBT1 = gbm::gbm(resp ~., data=Train.set, distribution='gaussian', n.trees=500
                    interaction.depth=4)
 summary(treeBT1)
 
-plot(treeBT1, i='productType')
-
 Train.fitted = predict(treeBT1, type='response')
 Test.pred = predict(treeBT1, Test.set, type='response')
 
@@ -276,7 +274,7 @@ plot(Train.fitted, as.matrix(Train.resp), xlab="Fitted", ylab="Actual")
 lines(Test.pred, as.matrix(Test.resp), type="p", col="red", pch=19)
 abline(0,1, col="red")
 
-library(caret)          # install.packages("caret")
+library(caret)         
 Boosting <- data.frame(
   tr.RMSE      = caret::RMSE(Train.fitted, as.matrix(Train.resp)),
   tr.Rsquare   = caret::R2(Train.fitted, Train.resp),
@@ -295,7 +293,6 @@ Boosting
 
 products2 <- products %>% na.omit()
 
-# Remove outliers that could corrupt model
 Q <- quantile(products2$resp, probs=c(.25, .75), na.rm = FALSE)
 iqr <- IQR(products2$resp)
 up <-  Q[2]+1.5*iqr # Upper Range
@@ -317,26 +314,106 @@ products4 <- tibble((products2 - products.means)/ products.SDs)
 products4
 
 Orig <- products4 
-train.size <- 8000            # num of rows for training set
-test.size <- 2523             # num of rows for testing set
-my.seed <- 1234              # give a seed
+sum(is.na(Orig))
+train.size <- 8000          
+test.size <- 2523           
+my.seed <- 1234           
 
 source('https://nmimoto.github.io/R/ML-00.txt')
 
 library(neuralnet)
 sigmoid <- function(x) 1 / (1 + exp(-x))
 
+layout(matrix(1:6, 2, 3, byrow=TRUE))    # to plot 5 in 1 page
+CVFitDiagnosis <- numeric(0)
+for (k in 1:5) {
+  
+  set.seed(my.seed)
+  Fit00 = neuralnet::neuralnet(resp ~.,            # <===== Try using different set of columns here
+                               CV.train[[k]],
+                               hidden=1,             # <===== Try different num of nodes here
+                               learningrate=1e-2,    # <===== For numerical problem try changing number here
+                               act.fct=sigmoid,
+                               linear.output=TRUE)   # This should be TRUE for regression
+  
+ 
+  Train.fitted = predict(Fit00, newdata=CV.train[[k]], type="vector")
+  Valid.fitted = predict(Fit00, newdata=CV.valid[[k]], type="vector")
+  
+  plot( Train.fitted, as.matrix(CV.train[[k]]$resp), xlab="Fitted", ylab="Actual",main=paste("K=",k))
+  lines(Valid.fitted, as.matrix(CV.valid[[k]]$resp), type="p", xlab="Fitted", ylab="Actual", col="red", pch=20)
+  abline(0,1)
+  
+  library(caret)          
+  CVFitDiagnosis1 <- data.frame(
+    tr.RMSE   = caret::RMSE(Train.fitted, as.matrix(CV.train[[k]]$resp)),
+    tr.Rsquare  = caret::R2(Train.fitted,           CV.train[[k]]$resp),
+    val.RMSE  = caret::RMSE(Valid.fitted, as.matrix(CV.valid[[k]]$resp)),
+    val.Rsquare = caret::R2(Valid.fitted,           CV.valid[[k]]$resp)
+  )
+  CVFitDiagnosis <- rbind(CVFitDiagnosis, CVFitDiagnosis1)
+}
+layout(1)
+
+CVFitDiagnosis
+Av.CVFitDiagnosis = apply(CVFitDiagnosis, 2, mean)
+Av.CVFitDiagnosis
 
 set.seed(my.seed)
 Fit01 = neuralnet::neuralnet(resp ~.,
                              Train.set,
-                             hidden=3,
+                             hidden=1,
                              learningrate=1e-2,
                              act.fct=sigmoid,
                              linear.output=TRUE)
-# linear.output FALSE means activation function is applied to output node
 
 summary(Fit01)
+
+plot(Fit01)
+
+Train.fitted = predict(Fit01, newdata=Train.set, type="vector")
+Test.fitted  = predict(Fit01, newdata=Test.set, type="vector")
+
+plot( Train.fitted, as.matrix(Train.set$resp), xlab="Fitted", ylab="Actual",main="Final Test.set Fit")
+lines(Test.fitted,  as.matrix(Test.set$resp), type="p", xlab="Fitted", ylab="Actual", col="red", pch=20)
+abline(0,1)
+
+library(caret)         
+FinalFitDiagnosis <- data.frame(
+  tr.RMSE   = caret::RMSE(Train.fitted,  as.matrix(Train.set$resp)),
+  tr.Rsquare  = caret::R2(Train.fitted,            Train.set$resp),
+  test.RMSE  = caret::RMSE(Test.fitted,  as.matrix(Test.set$resp)),
+  test.Rsquare = caret::R2(Test.fitted,            Test.set$resp)
+)
+FinalFitDiagnosis
+
+#Compare
+CVFitDiagnosis
+Av.CVFitDiagnosis
+
+Train.fitted = predict(Fit01, newdata=Train.set, type="vector")
+Test.fitted = predict(Fit01, newdata=Test.set, type="vector")
+Train.resp = Train.set$resp
+Test.resp = Test.set$resp
+
+Train.fitted.unscaled <- products.means$resp[1] + Train.fitted * products.SDs$resp[1]
+Train.resp.unscaled   <- products.means$resp[1] + Train.resp   * products.SDs$resp[1]
+Test.fitted.unscaled  <- products.means$resp[1] + Test.fitted  * products.SDs$resp[1]
+Test.resp.unscaled    <- products.means$resp[1] + Test.resp    * products.SDs$resp[1]
+
+plot( Train.fitted.unscaled, as.matrix(Train.resp.unscaled), xlab="Fitted", ylab="Actual",main="Final Test.set fit - Unscaled")
+lines(Test.fitted.unscaled,  as.matrix(Test.resp.unscaled), type="p", xlab="Fitted", ylab="Actual", col="red", pch=20)
+abline(0,1)
+
+library(caret)            # install.packages("caret")
+FinalFitDiagnosis.unsc <- data.frame(
+  tr.RMSE   = caret::RMSE(Train.fitted.unscaled, as.matrix(Train.resp.unscaled)),
+  tr.Rsquare  = caret::R2(Train.fitted.unscaled,           Train.resp.unscaled),
+  test.RMSE  = caret::RMSE(Test.fitted.unscaled,  as.matrix(Test.resp.unscaled)),
+  test.Rsquare = caret::R2(Test.fitted.unscaled,            Test.resp.unscaled)
+)
+FinalFitDiagnosis.unsc
+
 #Section 4b. Model Improvement (Optional. 0-2 slide)
 #  - Can you improve the prediction accuracy of the above model?
 #  - Tweaking of the model parameter
